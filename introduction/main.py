a82bd5b2f73ea32d2b0cd97c9c067233c63cf5c0
officialjwise <officialjwise20@gmail.com>
2024-06-11 16:06:16 +0000

    Basics and fundamentals of python programming

---
+++ introduction/main.py
@@ -0,0 +1,25 @@
+# Sum of two integers 
+x = 5
+y = 10
+print(x+y)
+
+# Multiple variables
+x,y,z = 1,2,3
+a = x+y+z
+print(a)
+
+age = 5
+age = 10
+print(age)
+
+cwa = 75.14 
+
+isGirl = True
+
+# Let's print our names 
+firstName = "Daniel"
+lastName = 'Kodua'
+middleName = 'Amoako'
+amoako = middleName
+fullName = amoako
+print("My name is " + firstName + "\n" + middleName + "\n" + lastName)
